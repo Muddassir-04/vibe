477c839d259e15bb8b41f5eafdb0c2e49c30387b
Muddassir-04 <muddassirahmed0526@gmail.com>
2025-08-26 00:06:32 +0530

    messages UI

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -2,14 +2,29 @@ import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 import { inngest } from "@/inngest/client";
+import { Input } from "@/components/ui/input";
 
 export const messagesRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  getMany: baseProcedure
+  .input(
+      z.object({
+        projectId: z.string().min(1, { message: "Project ID is required " }), // ✅ added
+      }),
+    )
+    .query(async ({ input }) => {
     const messages = await prisma.message.findMany({
+      where: {
+        projectId: input.projectId,
+      },
+      include: {
+        fragment: true,
+      },
       orderBy: {
-        updatedAt: "desc",
+        updatedAt: "asc",
       },
     });
+  
+  
 
     return messages;
   }),
