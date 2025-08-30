9cdc407e52b47624e20d746a45248d5c0f432d47
Muddassir-04 <muddassirahmed0526@gmail.com>
2025-08-30 16:59:27 +0530

    bug fixes

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -14,6 +14,7 @@ export const messagesRouter = createTRPCRouter({
       }),
     )
     .query(async ({ input, ctx }) => {
+
     const messages = await prisma.message.findMany({
       where: {
         projectId: input.projectId,
